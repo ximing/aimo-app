078910325bd8652ea9b417bc2d2cc76938b0da59
ximing <ximing@meituan.com>
2026-02-28 18:35:08 +0800

    chore(config): 将 app.json 配置迁移至 app.config.js

--- app.config.js
+++ app.config.js
@@ -3,13 +3,66 @@
  * 自动从 package.json 读取版本号，确保一致性
  */
 
-module.exports = function () {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  const packageJson = require("./package.json");
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const packageJson = require("./package.json");
 
-  return {
-    expo: {
-      version: packageJson.version,
+module.exports = {
+  expo: {
+    name: "aimo",
+    slug: "aimo-app",
+    version: packageJson.version,
+    orientation: "portrait",
+    icon: "./assets/logo.png",
+    scheme: "aimoapp",
+    userInterfaceStyle: "automatic",
+    ios: {
+      bundleIdentifier: "com.delu.aimo",
+      supportsTablet: true,
     },
-  };
+    android: {
+      package: "com.delu.aimo",
+      adaptiveIcon: {
+        backgroundColor: "#FFFFFF",
+        foregroundImage: "./assets/logo.png",
+        monochromeImage: "./assets/logo-dark.png",
+      },
+      predictiveBackGestureEnabled: false,
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/logo.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/logo.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000",
+            image: "./assets/logo-dark.png",
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "a9dd2767-f4a6-4412-9543-9e8a0d1595ae",
+      },
+    },
+    runtimeVersion: {
+      policy: "appVersion",
+    },
+    updates: {
+      url: "https://u.expo.dev/a9dd2767-f4a6-4412-9543-9e8a0d1595ae",
+    },
+  },
 };
